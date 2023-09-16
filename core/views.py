c57584ea888ebe89283381f6a44f4c9c346304f9
D-Valjean <fsistemas.tech@gmail.com>
2023-09-16 19:02:03 -0400

    Update Perfil

--- core/views.py
+++ core/views.py
@@ -4,21 +4,37 @@ from django.views.generic import TemplateView
 from django.contrib.auth.models import Group
 from .forms import RegisterForm
 from django.views import View
+from django.utils.decorators import method_decorator
 # Create your views here.
+
+
+def plural_to_singular(plural):
+    # Diccionario de palabras
+    plural_singular = {
+        'estudiantes': 'estudiante',
+        'profesores': 'profesor',
+        'director': 'director',
+        'administrativos': 'administrativo'
+    }
+
+    return plural_singular.get(plural, "error")
 
 
 class CustumTemplateView(TemplateView):
     group_name = None
+    group_name_singular = None
 
     def get_contex_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        ruser = self.request.user  # ruser = request.user from the request
-        group_name = None
-        if ruser.is_authenticated:
-            group = Group.objects.filter(user=ruser).first()
+        user = self.request.user  # ruser = request.user from the request
+        if user.is_authenticated:
+            group = Group.objects
             if group:
-                group_name = group.name
-        context['group_name'] = group_name
+                self.group_name = group.name
+                self.group_name_singular = plural_to_singular(group.name)
+        context['group_name'] = self.group_name
+        context['group_name_singular'] = self.group_name_singular
+
         return context
 
 
